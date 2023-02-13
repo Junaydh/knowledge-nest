128ef94ed01fd6660b87fe4e6fd69026ae652bb8
Colton Hyland <colthyland@gmail.com>
2023-02-13 13:04:17 -0800

    corrected comments seed

--- db/seeds/06_comments.sql
+++ db/seeds/06_comments.sql
@@ -1,19 +1,19 @@
 DELETE FROM comments;
 
-INSERT INTO comments
+INSERT INTO comments (owner_id, resource_id, comment)
 VALUES (1, 1, "Owner id 1 commented on resourse id 2");
 
-INSERT INTO comments
+INSERT INTO comments (owner_id, resource_id, comment)
 VALUES (1, 2, "Owner id 1 commented on resourse id 2");
 
-INSERT INTO comments
+INSERT INTO comments (owner_id, resource_id, comment)
 VALUES (2, 1, "Owner id 1 commented on resourse id 2");
 
-INSERT INTO comments
+INSERT INTO comments (owner_id, resource_id, comment)
 VALUES (2, 2, "Owner id 1 commented on resourse id 2");
 
-INSERT INTO comments
+INSERT INTO comments (owner_id, resource_id, comment)
 VALUES (3, 1, "Owner id 1 commented on resourse id 2");
 
-INSERT INTO comments
+INSERT INTO comments (owner_id, resource_id, comment)
 VALUES (3, 2, "Owner id 1 commented on resourse id 2");
