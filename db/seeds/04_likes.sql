8230ce8fdd51d18c2cfaa587238568939e78acf9
Colton Hyland <colthyland@gmail.com>
2023-02-13 12:50:19 -0800

    seeds lieks and comments added

---
+++ db/seeds/04_likes.sql
@@ -0,0 +1,19 @@
+DELETE FROM likes;
+
+INSERT INTO likes (owner_id, resource_id)
+VALUES (1, 1);
+
+INSERT INTO likes (owner_id, resource_id)
+VALUES (1, 2);
+
+INSERT INTO likes (owner_id, resource_id)
+VALUES (2, 1);
+
+INSERT INTO likes (owner_id, resource_id)
+VALUES (2, 2);
+
+INSERT INTO likes (owner_id, resource_id)
+VALUES (3, 3);
+
+INSERT INTO likes (owner_id, resource_id)
+VALUES (3, 3);
---
+++ db/seeds/06_comments.sql
@@ -0,0 +1,19 @@
+DELETE FROM comments;
+
+INSERT INTO comments
+VALUES (1, 1, "Owner id 1 commented on resourse id 2");
+
+INSERT INTO comments
+VALUES (1, 2, "Owner id 1 commented on resourse id 2");
+
+INSERT INTO comments
+VALUES (2, 1, "Owner id 1 commented on resourse id 2");
+
+INSERT INTO comments
+VALUES (2, 2, "Owner id 1 commented on resourse id 2");
+
+INSERT INTO comments
+VALUES (3, 1, "Owner id 1 commented on resourse id 2");
+
+INSERT INTO comments
+VALUES (3, 2, "Owner id 1 commented on resourse id 2");
