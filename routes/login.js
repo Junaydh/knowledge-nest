8799e04809fdcacb80ca3f6d3d0629f984082093
Adam Marx <adamalexmarx@gmail.com>
2023-02-14 17:51:47 -0800

    registration pages, with checks for repeated username/email

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const router  = express.Router();
 const cookieSession = require('cookie-session');
+const bcrypt = require('bcrypt');
+
+const app = express();
+
+app.use(cookieSession({
+  name: 'session',
+  keys: 'saKfsjfiAJmz92maxAJd2r4kHJAc647jfas7fg2uciGGGgfdSFS7'
+}));
 
 router.get('/', (req, res) => {
   res.render('users');
 });
+
+router.post('/', (req, res) => {
+
+})
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
+const cookieSession = require('cookie-session');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -26,12 +27,18 @@ app.use(
 );
 app.use(express.static('public'));
 
+app.use(cookieSession({
+  name: 'session',
+  keys: [process.env.SESSION_SECRET],
+}));
+
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
 const loginRoutes = require('./routes/login');
+const registrationRoutes = require('./routes/registration');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -40,6 +47,7 @@ app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 app.use('/login', loginRoutes);
+app.use('/register', registrationRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
