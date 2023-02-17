eab64909cb3348f5f3cd4a661ddbc1b21393ddc5
Junayd H <junaydhus@gmail.com>
2023-02-17 09:46:05 -0800

    add additional data to tags table

--- db/seeds/03_tags.sql
+++ db/seeds/03_tags.sql
@@ -6,3 +6,15 @@ INSERT INTO tags (resource_id, name)
 VALUES (2, 'fitness');
 INSERT INTO tags (resource_id, name)
 VALUES (3, 'gardening');
+INSERT INTO tags (resource_id, name)
+VALUES (1, 'recipe');
+INSERT INTO tags (resource_id, name)
+VALUES (3, 'botany');
+INSERT INTO tags (resource_id, name)
+VALUES (3, 'hydroponics');
+INSERT INTO tags (resource_id, name)
+VALUES (2, 'exercise');
+INSERT INTO tags (resource_id, name)
+VALUES (2, 'yoga');
+INSERT INTO tags (resource_id, name)
+VALUES (2, 'calisthenics');
